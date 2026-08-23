6fea2323be2d681ce43e29da0fe93bcc994cab1e
darkwebdev <darkwebdev@users.noreply.github.com>
2026-08-23 18:43:46 +0000

    deploy: 9f53b987672b3d73d3782d3ef8fd0de39db43172

---
+++ storybook/assets/index-CbfzPbbD.js
@@ -0,0 +1,1 @@
+import{R as e}from"./iframe-SCZB5zPH.js";import{P as n}from"./PullIcon-Cu4UA3-g.js";function o({children:l}){const t=parseFloat(l);return e.createElement(e.Fragment,null,e.createElement(n,null),"×",t)}o.__docgenInfo={description:"",methods:[],displayName:"Pulls"};export{o as P};

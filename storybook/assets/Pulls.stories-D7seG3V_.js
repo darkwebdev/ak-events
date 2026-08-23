925c8edbbf7a06bb7c53f293ab61511d14d3ec40
darkwebdev <darkwebdev@users.noreply.github.com>
2026-08-23 18:16:51 +0000

    deploy: 4f1df5612d12aee8f08884b47fc84849c727fb6b

---
+++ storybook/assets/Pulls.stories-D7seG3V_.js
@@ -0,0 +1,9 @@
+import{R as n}from"./iframe-D6caSHEe.js";import{a}from"./index-DQuEGokF.js";import"./preload-helper-C1FmrZbK.js";import"./images-BVwpUHA0.js";const D={title:"Components/Pulls",component:a};function e(){return n.createElement(a,null,"1")}function r(){return n.createElement(a,null,"2")}function o(){return n.createElement(a,null,"0")}function s(){return n.createElement(a,null,"2.5")}e.__docgenInfo={description:"",methods:[],displayName:"Singular"};r.__docgenInfo={description:"",methods:[],displayName:"Plural"};o.__docgenInfo={description:"",methods:[],displayName:"Zero"};s.__docgenInfo={description:"",methods:[],displayName:"Decimal"};var t,l,c;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`function Singular() {
+  return <Pulls>1</Pulls>;
+}`,...(c=(l=e.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var u,i,m;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`function Plural() {
+  return <Pulls>2</Pulls>;
+}`,...(m=(i=r.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,d,f;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`function Zero() {
+  return <Pulls>0</Pulls>;
+}`,...(f=(d=o.parameters)==null?void 0:d.docs)==null?void 0:f.source}}};var P,g,_;s.parameters={...s.parameters,docs:{...(P=s.parameters)==null?void 0:P.docs,source:{originalSource:`function Decimal() {
+  return <Pulls>2.5</Pulls>;
+}`,...(_=(g=s.parameters)==null?void 0:g.docs)==null?void 0:_.source}}};const I=["Singular","Plural","Zero","Decimal"];export{s as Decimal,r as Plural,e as Singular,o as Zero,I as __namedExportsOrder,D as default};

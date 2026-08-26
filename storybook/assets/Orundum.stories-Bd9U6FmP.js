d64296126629b285738ee2c6af064f2d0fa7db15
darkwebdev <darkwebdev@users.noreply.github.com>
2026-08-26 12:15:27 +0000

    deploy: c46090427accc379d9eb18f8c14a1a61d10b96ae

---
+++ storybook/assets/Orundum.stories-Bd9U6FmP.js
@@ -0,0 +1,29 @@
+import{R as e}from"./iframe-B3oq4QGZ.js";import{O as r}from"./index-XCKGDxwU.js";import"./preload-helper-C1FmrZbK.js";import"./index-Pus8gx1u.js";import"./PullIcon-DekuEoqT.js";import"./images-BVwpUHA0.js";import"./OrundumIcon-46EPSg-2.js";const $={title:"Components/Orundum",component:r};function n(){return e.createElement(r,null,"1200")}function u(){return e.createElement(r,{withPulls:!0},"1200")}function o(){return e.createElement(r,{withPulls:!1},"1200")}function t(){return e.createElement(r,{withPulls:!0},"300")}function s(){return e.createElement(r,{withPulls:!0},"3600")}function a(){return e.createElement(r,{withPulls:!0,pullsPrecision:3},"100")}function i(){return e.createElement(r,{withPulls:!0,pullsPrecision:1},"3600")}function m(){return e.createElement(r,null)}function c(){return e.createElement(r,null,"0")}function l(){return e.createElement(r,{withPulls:!0},"0")}function d(){return e.createElement(r,{withPulls:!0,pullsPrecision:1},"0")}n.__docgenInfo={description:"",methods:[],displayName:"Default"};u.__docgenInfo={description:"",methods:[],displayName:"WithPulls"};o.__docgenInfo={description:"",methods:[],displayName:"NoPulls"};t.__docgenInfo={description:"",methods:[],displayName:"SmallAmount"};s.__docgenInfo={description:"",methods:[],displayName:"LargeAmount"};a.__docgenInfo={description:"",methods:[],displayName:"SmallAmountWithPrecision"};i.__docgenInfo={description:"",methods:[],displayName:"LargeAmountWithPrecision"};m.__docgenInfo={description:"",methods:[],displayName:"NoOrundum"};c.__docgenInfo={description:"",methods:[],displayName:"ZeroOrundum"};l.__docgenInfo={description:"",methods:[],displayName:"ZeroOrundumWithPulls"};d.__docgenInfo={description:"",methods:[],displayName:"ZeroOrundumWithPrecision"};var p,h,P;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`function Default() {
+  return <Orundum>1200</Orundum>;
+}`,...(P=(h=n.parameters)==null?void 0:h.docs)==null?void 0:P.source}}};var f,O,g;u.parameters={...u.parameters,docs:{...(f=u.parameters)==null?void 0:f.docs,source:{originalSource:`function WithPulls() {
+  return <Orundum withPulls>1200</Orundum>;
+}`,...(g=(O=u.parameters)==null?void 0:O.docs)==null?void 0:g.source}}};var _,W,N;o.parameters={...o.parameters,docs:{...(_=o.parameters)==null?void 0:_.docs,source:{originalSource:`function NoPulls() {
+  return <Orundum withPulls={false}>1200</Orundum>;
+}`,...(N=(W=o.parameters)==null?void 0:W.docs)==null?void 0:N.source}}};var S,w,A;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:`function SmallAmount() {
+  return <Orundum withPulls>300</Orundum>;
+}`,...(A=(w=t.parameters)==null?void 0:w.docs)==null?void 0:A.source}}};var E,Z,y;s.parameters={...s.parameters,docs:{...(E=s.parameters)==null?void 0:E.docs,source:{originalSource:`function LargeAmount() {
+  return <Orundum withPulls>3600</Orundum>;
+}`,...(y=(Z=s.parameters)==null?void 0:Z.docs)==null?void 0:y.source}}};var I,L,D;a.parameters={...a.parameters,docs:{...(I=a.parameters)==null?void 0:I.docs,source:{originalSource:`function SmallAmountWithPrecision() {
+  return <Orundum withPulls pullsPrecision={3}>
+      100
+    </Orundum>;
+}`,...(D=(L=a.parameters)==null?void 0:L.docs)==null?void 0:D.source}}};var x,R,C;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:`function LargeAmountWithPrecision() {
+  return <Orundum withPulls pullsPrecision={1}>
+      3600
+    </Orundum>;
+}`,...(C=(R=i.parameters)==null?void 0:R.docs)==null?void 0:C.source}}};var b,j,k;m.parameters={...m.parameters,docs:{...(b=m.parameters)==null?void 0:b.docs,source:{originalSource:`function NoOrundum() {
+  return <Orundum />;
+}`,...(k=(j=m.parameters)==null?void 0:j.docs)==null?void 0:k.source}}};var q,v,z;c.parameters={...c.parameters,docs:{...(q=c.parameters)==null?void 0:q.docs,source:{originalSource:`function ZeroOrundum() {
+  return <Orundum>0</Orundum>;
+}`,...(z=(v=c.parameters)==null?void 0:v.docs)==null?void 0:z.source}}};var B,F,G;l.parameters={...l.parameters,docs:{...(B=l.parameters)==null?void 0:B.docs,source:{originalSource:`function ZeroOrundumWithPulls() {
+  return <Orundum withPulls>0</Orundum>;
+}`,...(G=(F=l.parameters)==null?void 0:F.docs)==null?void 0:G.source}}};var H,J,K;d.parameters={...d.parameters,docs:{...(H=d.parameters)==null?void 0:H.docs,source:{originalSource:`function ZeroOrundumWithPrecision() {
+  return <Orundum withPulls pullsPrecision={1}>
+      0
+    </Orundum>;
+}`,...(K=(J=d.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};const rr=["Default","WithPulls","NoPulls","SmallAmount","LargeAmount","SmallAmountWithPrecision","LargeAmountWithPrecision","NoOrundum","ZeroOrundum","ZeroOrundumWithPulls","ZeroOrundumWithPrecision"];export{n as Default,s as LargeAmount,i as LargeAmountWithPrecision,m as NoOrundum,o as NoPulls,t as SmallAmount,a as SmallAmountWithPrecision,u as WithPulls,c as ZeroOrundum,d as ZeroOrundumWithPrecision,l as ZeroOrundumWithPulls,rr as __namedExportsOrder,$ as default};
---
+++ storybook/assets/index-Pus8gx1u.js
@@ -0,0 +1,1 @@
+import{R as e}from"./iframe-B3oq4QGZ.js";import{P as n}from"./PullIcon-DekuEoqT.js";function o({children:l}){const t=parseFloat(l);return e.createElement(e.Fragment,null,e.createElement(n,null),"×",t)}o.__docgenInfo={description:"",methods:[],displayName:"Pulls"};export{o as P};
